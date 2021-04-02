cf8094bb1e9af19580dfb7119411deaa720b398f
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-02 10:57:54 +0100

    Dice initializes with default of 6 sides, and can initialize with argument

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,11 @@
 class Dice
 
+  attr_reader :sides
+
+  def initialize(sides=6)
+    @sides = sides
+  end
+
   def roll(number)
     rand(1..6)
   end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -6,6 +6,20 @@ describe Dice do
     is_expected.to be_instance_of Dice
   end
 
+  describe 'initialize' do
+
+    it 'initializes with a default of 6 sides' do
+      expect(subject.sides).to eq(6)
+    end
+
+    it 'initializes with a given number of sides' do
+      dice = Dice.new(9)
+      expect(dice.sides).to eq(9)
+    end
+
+  end
+
+
   it { is_expected.to respond_to :roll }
 
   it 'should return a number between 1 and 6 when rolled' do
